a5be2d90e1276123060a5aa9f24b45358973b05d
Simone Gasbarroni <simone.gasbarroni@axant.it>
2013-12-11 11:43:35 +0100

    removed helpers

--- tgext.ecommerce/tgextecommerce/helpers.py
+++ tgext.ecommerce/tgextecommerce/helpers.py
@@ -1,8 +1,2 @@
 # -*- coding: utf-8 -*-
 
-"""WebHelpers used in tgext.ecommerce."""
-
-from webhelpers import date, feedgenerator, html, number, misc, text
-
-def bold(text):
-    return html.literal('<strong>%s</strong>' % text)
