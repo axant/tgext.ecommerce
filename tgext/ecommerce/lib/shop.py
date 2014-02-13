ae13859b8ce8064de997170702bd6672906f8518
Simone Marzola <marzolasimone@gmail.com>
2014-02-13 19:51:48 +0100

    use sphinx style for docstrings

--- tgext/ecommerce/lib/shop.py
+++ tgext/ecommerce/lib/shop.py
@@ -271,22 +271,22 @@ class ShopManager(object):
         models.DBSession.flush()
         return order
 
-    def get_order(self,_id):
+    def get_order(self, _id):
         return models.Order.query.get(_id=ObjectId(_id))
 
     def get_user_orders(self, user_id):
-        '''Retrieves all the past orders of a given user
+        """Retrieves all the past orders of a given user
 
-        @param user_id: the user id string to filter for
-        '''
+        :param user_id: the user id string to filter for
+        """
         return models.Order.query.find({'user_id': user_id})
 
     def get_suggested_products_per_user(self, user_id, limit=5):
-        '''Gives a list of suggested sku products based on the past orders of a user
+        """Gives a list of suggested sku products based on the past orders of a user
 
-        @param user_id: the user id string to get suggestions for
-        @parmas limit: optional max number of suggestions (default to 5)
-        '''
+        :param user_id: the user id string to get suggestions for
+        :parmas limit: optional max number of suggestions (default to 5)
+        """
         past_orders = models.Order.query.find({'user_id': user_id})
         skus = Counter([item.sku for order in past_orders for item in order.items])
         suggested_skus = skus.most_common(limit)
