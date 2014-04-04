05fbbd9db4dd628ee4689f34efb5e15ba13eee2a
Simone Gasbarroni <simone.gasbarroni@axant.it>
2014-04-04 17:05:41 +0200

    added order id to paypal payments request, it's very ugly, but before it wasn't better, need a refactor ASAP

--- tgext/ecommerce/lib/payments/paypal.py
+++ tgext/ecommerce/lib/payments/paypal.py
@@ -36,7 +36,7 @@ def pay(cart, redirection_url, cancel_url):
             "return_url": redirection_url,
             "cancel_url": cancel_url
         },
-        "transactions": [{"item_list": {"items": [{"name": "Tavolaclandestina",
+        "transactions": [{"item_list": {"items": [{"name": "Tavolaclandestina order number: %s" % str(cart._id),
                                                    "price": '%0.2f' % (cart.total + cart.order_info.shipping_charges +
                                                                        total_discount),
                                                    "sku": 'TC',
