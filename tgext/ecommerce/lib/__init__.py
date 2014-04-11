0d9f36984027c1b3a06070ae6141517acbf56d39
Simone Gasbarroni <simone.gasbarroni@axant.it>
2014-04-11 12:27:26 +0200

    fixed default edit order form

--- tgext/ecommerce/lib/__init__.py
+++ tgext/ecommerce/lib/__init__.py
@@ -6,7 +6,7 @@ def get_edit_order_form():
     ecommerce_config = config['_pluggable_ecommerce_config']
     edit_order_form = ecommerce_config.get('edit_order_form_instance')
     if not edit_order_form:
-        form_path = ecommerce_config.get('edit_order_form', 'ecommerce.lib.forms.DefaultEditOrderForm')
+        form_path = ecommerce_config.get('edit_order_form', 'tgext.ecommerce.lib.forms.DefaultEditOrderForm')
         module, form_name = form_path.rsplit('.', 1)
         module = __import__(module, fromlist=form_name)
         form_class = getattr(module, form_name)
